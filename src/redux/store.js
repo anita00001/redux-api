f2c79160dc42efe322b7c69cec28d13150e4bcf3
Anita Sharma <sharma.anita00001@gmail.com>
2023-07-19 11:35:24 +0545

    Store to take users reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './userSlice'
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: '',
   },
 })
 
